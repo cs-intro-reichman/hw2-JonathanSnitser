208dd63d2337cdb3e57580cc186dd85f999fa3ac
unknown <snitser.yonatan@post.runi.ac.il>
2024-11-14 23:11:41 +0200

    completed

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,16 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
+	    int termNum = Integer.parseInt(args[0]);
+		double sum = 1.0;
+		double devider = 3;
+		double plusOtMinus = -1;
+		for (int i=0; i < termNum; i++) {
+			sum = sum + (1 /devider * plusOtMinus);
+			devider = devider + 2;
+			plusOtMinus = plusOtMinus * (-1);
+		}
+		System.out.println("pi according to Java: " + Math.PI);
+		System.out.println("pi, approximated: " + (sum * 4));
 	}
 }
--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,23 @@
-// Prints a crowd cheering output.
+
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+	    String cheer = args[0];
+            int times = Integer.parseInt(args[1]);
+            String letters = "AEFHILMNORSX";
+
+            int i = 0;
+            while  (i < cheer.length() - 1 ) {
+                char c = cheer.charAt(i);
+                if (letters.indexOf(c) != -1) {
+                    System.out.println("Give me an " + c + ": " + c + "!");
+                } else {
+                    System.out.println("Give me a  " + c + ": " + c + "!");
+                }
+                i++;
+            }
+            System.out.println( "What does that spell?");
+            for (int j = 0; j < times; j++) {
+                System.out.println(cheer + "!!!");
+            }
         }
 }
