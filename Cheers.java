4641147c79ec57babb3581efc6c8ff03c45ece5f
unknown <snitser.yonatan@post.runi.ac.il>
2024-11-18 09:32:06 +0200

    now shows the last character

--- Cheers.java
+++ Cheers.java
@@ -2,17 +2,21 @@
 public class Cheers {
         public static void main(String[] args) {
 	    String cheer = args[0];
-            int times = Integer.parseInt(args[1]);
-            String letters = "AEFHILMNORSX";
+        int times = Integer.parseInt(args[1]);
+        String bigLetters = "AEFHILMNORSX"; //// probably the strings
+        String smallLetters = "aefhilmnorsx";
 
             int i = 0;
-            while  (i < cheer.length() - 1 ) {
+            while  (i < cheer.length() ) {
                 char c = cheer.charAt(i);
-                if (letters.indexOf(c) != -1) {
+                boolean isBigLet = bigLetters.indexOf(c) != -1;
+                boolean isSmallLet = smallLetters.indexOf(c) != -1;
+                if (isBigLet || isSmallLet) {
                     System.out.println("Give me an " + c + ": " + c + "!");
                 } else {
                     System.out.println("Give me a  " + c + ": " + c + "!");
                 }
+                
                 i++;
             }
             System.out.println( "What does that spell?");
@@ -20,4 +24,4 @@ public class Cheers {
                 System.out.println(cheer + "!!!");
             }
         }
-}
+}   
